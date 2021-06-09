64325583f9edbbad507f76f7ef79545e357403b2
toyboot4e <toyboot4e@gmail.com>
2021-06-09 20:02:47 +0900

    return Err on empty input

--- src/lex.rs
+++ src/lex.rs
@@ -2,6 +2,8 @@
 `&str` -> `Vec<Token>`
 */
 
+use thiserror::Error;
+
 use crate::span::ByteSpan;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
@@ -27,7 +29,38 @@ pub struct Token {
 }
 
 impl Token {
-    pub fn slice(&self, src: &str) -> &str {
+    pub fn slice<'a>(&self, src: &'a str) -> &'a str {
         self.span.slice(src)
+    }
+}
+
+#[derive(Debug, Clone, Error)]
+pub enum TokenizeError {
+    #[error("given empty input")]
+    GivenEmptyInput,
+}
+
+pub struct Tokenizer<'a> {
+    src: &'a [u8],
+    /// Span of the source code currently handled
+    sp: ByteSpan,
+}
+
+impl<'a> Tokenizer<'a> {
+    pub fn tokenize(src: &str) -> Result<Vec<Token>, TokenizeError> {
+        let mut me = Tokenizer {
+            src: src.as_bytes(),
+            sp: Default::default(),
+        };
+
+        me.tokenize_impl()
+    }
+
+    fn tokenize_impl(&mut self) -> Result<Vec<Token>, TokenizeError> {
+        if self.src.is_empty() {
+            return Err(TokenizeError::GivenEmptyInput);
+        }
+
+        todo!()
     }
 }
--- src/span.rs
+++ src/span.rs
@@ -2,13 +2,14 @@
 Byte-based span of source text
 */
 
+#[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Hash)]
 pub struct ByteSpan {
     pub lo: usize,
     pub hi: usize,
 }
 
 impl ByteSpan {
-    pub fn slice(&self, src: &str) -> &str {
-        src[self.lo..self.hi]
+    pub fn slice<'a>(&self, src: &'a str) -> &'a str {
+        &src[self.lo..self.hi]
     }
 }
