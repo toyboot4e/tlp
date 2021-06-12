9dd9bb675f11ba72eb8f39cdb4a15e8d8842ef3d
toyboot4e <toyboot4e@gmail.com>
2021-06-12 16:08:23 +0900

    lex number and identifier

--- src/lex.rs
+++ src/lex.rs
@@ -6,11 +6,15 @@ mod tk;
 
 pub use tk::{Token, TokenKind};
 
+use thiserror::Error;
+
 use crate::span::ByteSpan;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Error)]
 pub enum LexError {
-    //
+    // TODO: use line:column representation
+    #[error("It doesn't make any sense: {sp:?}")]
+    Unreachable { sp: ByteSpan },
 }
 
 pub fn lex(src: &str) -> Result<Vec<Token>, LexError> {
@@ -39,13 +43,23 @@ fn is_ws(c: u8) -> bool {
 }
 
 #[inline(always)]
-fn is_digit(c: u8) -> bool {
+fn is_num_start(c: u8) -> bool {
     !(c < b'0' || b'9' < c)
 }
 
 #[inline(always)]
 fn is_num_body(c: u8) -> bool {
-    is_digit(c) || matches!(c, b'.' | b'E' | b'_')
+    is_num_start(c) || matches!(c, b'.' | b'E' | b'_')
+}
+
+#[inline(always)]
+fn is_ident_body(c: u8) -> bool {
+    !is_ws(c)
+}
+
+#[inline(always)]
+fn is_ident_start(c: u8) -> bool {
+    !(is_ws(c) || is_num_start(c))
 }
 
 #[inline(always)]
@@ -70,6 +84,41 @@ impl<'a> Lexer<'a> {
     fn consume_span_as(&mut self, kind: TokenKind) -> Token {
         let sp = self.consume_span();
         Token { sp, kind }
+    }
+
+    /// The predicate returns if we should continue scanning reading a byte
+    #[inline(always)]
+    fn advance_if(&mut self, p: &impl Fn(u8) -> bool) -> Option<()> {
+        if self.sp.hi >= self.src.len() {
+            return None;
+        }
+
+        let peek = self.src[self.sp.hi];
+
+        if p(peek) {
+            self.sp.hi += 1;
+            Some(())
+        } else {
+            None
+        }
+    }
+
+    /// The predicate returns if we should continue scanning reading a byte
+    #[inline(always)]
+    fn advance_while(&mut self, p: &impl Fn(u8) -> bool) {
+        loop {
+            if self.sp.hi >= self.src.len() {
+                return;
+            }
+
+            let peek = self.src[self.sp.hi];
+
+            if !p(peek) {
+                return;
+            }
+
+            self.sp.hi += 1;
+        }
     }
 }
 
@@ -111,7 +160,17 @@ impl<'a> Lexer<'a> {
             return Ok(());
         }
 
-        todo!()
+        if let Some(tk) = self.lex_num() {
+            self.tks.push(tk);
+            return Ok(());
+        }
+
+        if let Some(tk) = self.lex_ident() {
+            self.tks.push(tk);
+            return Ok(());
+        }
+
+        Err(LexError::Unreachable { sp: self.sp })
     }
 
     #[inline(always)]
@@ -128,53 +187,25 @@ impl<'a> Lexer<'a> {
 
     #[inline(always)]
     fn lex_ws(&mut self) -> Option<Token> {
-        let c = self.src[self.sp.hi];
-        if !is_ws(c) {
-            return None;
-        }
-
-        while self.sp.hi < self.src.len() {
-            self.sp.hi += 1;
-
-            let c = &self.src[self.sp.hi];
-
-            if !self::is_ws(*c) {
-                break;
-            }
-        }
-
+        self.advance_if(&self::is_ws)?;
+        self.advance_while(&self::is_ws);
         Some(self.consume_span_as(TokenKind::Ws))
     }
 
     /// [0-9]<num>*
     #[inline(always)]
     fn lex_num(&mut self) -> Option<Token> {
-        let current = self.src[self.sp.hi];
-        if !self::is_digit(current) {
-            return None;
-        }
-        todo!()
+        self.advance_if(&self::is_num_start)?;
+        self.advance_while(&self::is_num_body);
+        Some(self.consume_span_as(TokenKind::Num))
     }
 
-    /// [^0-9]<ws>*
+    /// [^<ws>]*
     #[inline(always)]
     fn lex_ident(&mut self) -> Option<Token> {
-        let c = self.src[self.sp.lo];
-        if !is_ws(c) {
-            return None;
-        }
-
-        while self.sp.hi < self.src.len() {
-            self.sp.hi += 1;
-
-            let c = &self.src[self.sp.hi];
-
-            if !self::is_ws(*c) {
-                break;
-            }
-        }
-
-        Some(self.consume_span_as(TokenKind::Ws))
+        self.advance_if(&self::is_ident_start)?;
+        self.advance_while(&self::is_ident_body);
+        Some(self.consume_span_as(TokenKind::Ident))
     }
 }
 
@@ -227,6 +258,49 @@ mod test {
                 Token {
                     kind: TokenKind::ParenClose,
                     sp: ByteSpan { lo: 3, hi: 4 },
+                },
+            ]
+        );
+
+        Ok(())
+    }
+
+    #[test]
+    fn num() -> Result<(), LexError> {
+        let src = "(* 1 3)";
+        //         0 2 4 6
+        let tks = crate::lex::lex(src)?;
+
+        assert_eq!(
+            tks,
+            vec![
+                Token {
+                    kind: TokenKind::ParenOpen,
+                    sp: ByteSpan { lo: 0, hi: 1 },
+                },
+                Token {
+                    kind: TokenKind::Ident,
+                    sp: ByteSpan { lo: 1, hi: 2 },
+                },
+                Token {
+                    kind: TokenKind::Ws,
+                    sp: ByteSpan { lo: 2, hi: 3 },
+                },
+                Token {
+                    kind: TokenKind::Num,
+                    sp: ByteSpan { lo: 3, hi: 4 },
+                },
+                Token {
+                    kind: TokenKind::Ws,
+                    sp: ByteSpan { lo: 4, hi: 5 },
+                },
+                Token {
+                    kind: TokenKind::Num,
+                    sp: ByteSpan { lo: 5, hi: 6 },
+                },
+                Token {
+                    kind: TokenKind::ParenClose,
+                    sp: ByteSpan { lo: 6, hi: 7 },
                 },
             ]
         );
