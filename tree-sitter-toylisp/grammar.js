e191591189602ec9914406e438839cfef11ee0e9
toyboot4e <toyboot4e@gmail.com>
2021-06-28 23:58:32 +0900

    add WIP tree-sitter-toylisp

---
+++ tree-sitter-toylisp/grammar.js
@@ -0,0 +1,23 @@
+const WS_CHARS =
+      /[\f\n\r\t, \u000B\u001C\u001D\u001E\u001F\u2028\u2029\u1680\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2008\u2009\u200a\u205f\u3000]/;
+
+const WS =
+      token(repeat1(WS_CHARS));
+
+const COMMENT =
+      token(/(-|#!).*\n?/);
+
+const STRING =
+      token(seq('"',
+                repeat(/[^"\\]/),
+                '"'));
+
+module.exports = grammar({
+    name: 'ToyLisp',
+    rules: {
+        source: $ => repeat(choice($._ws, $.comment, $.string)),
+        _ws: $ => WS,
+        comment: $ => COMMENT,
+        string: $ => STRING,
+    }
+});
