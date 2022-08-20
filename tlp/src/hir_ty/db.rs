5b90ea36f2834cf718fcb76b9f0d2683a3ed27f5
toyboot4e <toyboot4e@gmail.com>
2022-08-20 22:35:11 +0900

    wip(`hir_ty`): outline inference query

--- tlp/src/hir_ty/db.rs
+++ tlp/src/hir_ty/db.rs
@@ -1,8 +1,11 @@
 //! Incremental computation powered by [`salsa`]
 
-use std::fmt;
+use std::{sync::Arc, fmt};
 
-use crate::hir_def::db::*;
+use crate::{
+    hir_def::{db::*, ids::*, item_list::item},
+    hir_ty::infer::{self, InferenceResult},
+};
 
 /// [`salsa`] database instance for the queries
 #[salsa::database(SourceDB, ParseDB, InternDB, DefDB, HirDB)]
@@ -27,5 +30,6 @@ impl Upcast<dyn Intern> for DB {
 
 #[salsa::query_group(HirDB)]
 pub trait Hir: Def + Parse + Intern + Upcast<dyn Intern> {
-    //
+    #[salsa::invoke(infer::infer_proc_query)]
+    fn infer_proc_body(&self, proc_loc_id: HirItemLocId<item::DefProc>) -> Arc<InferenceResult>;
 }
--- tlp/src/hir_ty/infer.rs
+++ tlp/src/hir_ty/infer.rs
@@ -1,8 +1,6 @@
 //! Walks through procedure body and assigns types to each exrpression and pattern
 
-#![allow(unused)]
-
-use std::ops;
+use std::{ops, sync::Arc};
 
 use la_arena::{ArenaMap, Idx};
 
@@ -15,8 +13,27 @@ use crate::{
     hir_ty::{db::*, ty::Ty},
 };
 
+/// Infers
+pub(crate) fn infer_proc_query(
+    db: &dyn Hir,
+    proc: HirItemLocId<item::DefProc>,
+) -> Arc<InferenceResult> {
+    let body = db.proc_body(proc);
+    let unknown_ty = 0; // FIXME
+
+    let mut infer = Infer {
+        result: InferenceResult::new(unknown_ty),
+        db,
+        body: &body,
+    };
+
+    infer.infer_proc_body();
+
+    Arc::new(infer.result)
+}
+
 /// Mapping of expressions and patterns to types
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct InferenceResult {
     expr_types: ArenaMap<Idx<Expr>, Ty>,
     /// For each pattern record the type it resolves to.
@@ -24,7 +41,6 @@ pub struct InferenceResult {
     /// **Note**: When a pattern type is resolved it may still contain
     /// unresolved or missing subpatterns or subpatterns of mismatched types.
     pat_types: ArenaMap<Idx<Pat>, Ty>,
-
     unknown_ty: Ty,
 }
 
@@ -44,18 +60,57 @@ impl ops::Index<Idx<Pat>> for InferenceResult {
     }
 }
 
-/// Infers
-pub(crate) fn infer_proc(db: &dyn Hir, proc: HirItemLocId<item::DefProc>) -> InferenceResult {
-    todo!()
+impl InferenceResult {
+    fn new(unknown_ty: Ty) -> Self {
+        Self {
+            expr_types: Default::default(),
+            pat_types: Default::default(),
+            unknown_ty,
+        }
+    }
 }
 
 /// The inference context contains all information needed during type inference.
 #[derive(Clone)]
 pub(crate) struct Infer<'a> {
+    /// Output
+    pub(crate) result: InferenceResult,
     pub(crate) db: &'a dyn Hir,
     // pub(crate) owner: DefWithBodyId,
     pub(crate) body: &'a Body,
     // pub(crate) resolver: Resolver,
-    pub(crate) result: InferenceResult,
-    return_ty: Ty,
+    // return_ty: Ty,
+}
+
+/// Write result
+impl<'a> Infer<'a> {
+    fn write_expr_ty(&mut self, expr: Idx<Expr>, ty: Ty) {
+        self.result.expr_types.insert(expr, ty);
+    }
+
+    fn write_pat_ty(&mut self, pat: Idx<Pat>, ty: Ty) {
+        self.result.pat_types.insert(pat, ty);
+    }
+}
+
+impl<'a> Infer<'a> {
+    pub fn infer_proc_body(&mut self) {
+        let root = self.body.root_block;
+        self.infer_expr(root);
+    }
+}
+
+/// Infer expression types
+impl<'a> Infer<'a> {
+    pub fn infer_expr(&mut self, expr_idx: Idx<Expr>) {
+        let expr = &self.body.exprs[expr_idx];
+        match expr {
+            Expr::Block(block) => {
+                todo!()
+            }
+            Expr::Literal(lit) => todo!("{:?}", lit),
+            Expr::Let(let_) => todo!("{:?}", let_),
+            _ => todo!("{:?}", expr),
+        }
+    }
 }
