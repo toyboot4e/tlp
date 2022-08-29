bba042685878daae51aea757ddd5e9dc50f78013
toyboot4e <toyboot4e@gmail.com>
2022-08-29 21:22:41 +0900

    wip collect items

--- tlp-salsa-2022/src/ir.rs
+++ tlp-salsa-2022/src/ir.rs
@@ -3,7 +3,7 @@
 pub mod item;
 pub mod jar;
 
-use crate::base;
+use crate::{base, syntax::ast};
 
 #[salsa::jar(db = IrDb)]
 pub struct Jar(parse, jar::ParsedFile, item::Proc);
@@ -23,11 +23,22 @@ pub fn parse(db: &dyn IrDb, file: base::jar::InputFile) -> jar::ParsedFile {
     let mut items = Vec::new();
 
     let src = file.source_text(db.as_base_db());
-    let parse = crate::syntax::ast::parse(src);
+    let parse = ast::parse(src);
     let ast = parse.doc;
 
     for item in ast.items() {
-        todo!()
+        let item = match item {
+            ast::Item::DefProc(ast) => {
+                let proc = match item::Proc::from_ast(db, file, ast) {
+                    Some(x) => x,
+                    None => continue,
+                };
+
+                item::Item::Proc(proc)
+            }
+        };
+
+        items.push(item);
     }
 
     jar::ParsedFile::new(db, file, items)
--- tlp-salsa-2022/src/ir/item.rs
+++ tlp-salsa-2022/src/ir/item.rs
@@ -2,6 +2,7 @@
 
 use crate::{
     base::{
+        self,
         jar::{SpannedWord, Word},
         span::*,
     },
@@ -62,6 +63,38 @@ pub struct Proc {
     span: FileSpan,
     /// Unparsed body code
     ast: ast::DefProc,
+}
+
+impl Proc {
+    pub fn from_ast(
+        db: &dyn ir::IrDb,
+        file: base::jar::InputFile,
+        ast: ast::DefProc,
+    ) -> Option<Self> {
+        let name = {
+            // here we require function name for simplicity
+            let name_tk = ast.name()?;
+            let name = Word::intern(db.as_base_db(), name_tk.token().text());
+
+            let name_span = Span::from_rowan_range(name_tk.syn.text_range());
+            let name_span = FileSpan {
+                input_file: file,
+                start: name_span.start,
+                end: name_span.end,
+            };
+
+            SpannedWord::new(db.as_base_db(), name, name_span)
+        };
+
+        let file_span = Span::from_rowan_range(ast.syn.text_range());
+        let file_span = FileSpan {
+            input_file: file,
+            start: file_span.start,
+            end: file_span.end,
+        };
+
+        Some(Proc::new(db, name, file_span, ast))
+    }
 }
 
 impl<Db: ?Sized + ir::IrDb> salsa::DebugWithDb<Db> for Proc {
